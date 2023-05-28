a1f301b8438ab5b85ed558186a616cd949f2f0e8
Ali Beikverdi <ali@bitholla.com>
2023-05-28 20:30:21 +0900

    setup cleanups

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@ setup(
     name='hollaex-py-lib',
     version='1',
     license='MIT',
-    author="Mahdi",
-    author_email='mahdi@hollaex.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/hollaex/hollaex-py-lib',
     keywords='Hollaex',
     install_requires=[
           
-      ],
+    ],
 
 )
